7d2504543b95bb85c3e987028b62adffeb724046
Oscar Bolmsten <oscar.bolmsten@persequor.com>
2020-01-21 13:56:13 +0100

    Automatically update mirror

---
+++ .github/workflows/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+shopt -s extglob
+
+echo "Fetch manifest..."
+wget --no-verbose -O manifest.txt https://decentespresso.com/download/sync/de1plus/manifest.txt
+
+echo "Checking if there are differences..."
+git diff --exit-code manifest.txt
+
+if [ $? -eq 0 ]
+then
+  echo "No changes!"
+else
+  echo "Updates available!"
+
+  echo "Removing all source files..."
+  rm -rv !(README.md|manifest.txt|.git*)
+
+  echo "Creating list of files to download..."
+  grep -Eo '"(.*)"' manifest.txt | cut -d '"' -f2 | sed 's/^/https:\/\/decentespresso.com\/download\/sync\/de1plus\//' > files.txt
+
+  echo "Fetching all the files from manifest.txt..."
+  wget --no-verbose --force-directories --no-host-directories --cut-dirs=3 --input-file=files.txt
+
+  echo "Fetching new timestamp.txt..."
+  wget --no-verbose https://decentespresso.com/download/sync/de1plus/timestamp.txt
+
+  echo "Removing temporary filelist..."
+  rm files.txt
+
+  echo "Committing to git..."
+  git config --local user.email "mirror@decentespresso.com"
+  git config --local user.name "Decent Espresso"
+  git add --all
+  git commit --all --message="$(printf '%(%F %T %Z)T\n' $(cat timestamp.txt))"
+  git status
+  git push
+fi

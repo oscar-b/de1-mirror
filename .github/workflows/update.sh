7c32fdfdc339a41b200115db3cab42508b92ebf1
Oscar Bolmsten <oscar.bolmsten@persequor.com>
2020-01-24 20:30:07 +0100

    Better error handling in bash I hate you, bash.

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+set -e
 shopt -s extglob
 
 echo "Fetch manifest..."
 wget --no-verbose -O manifest.txt https://decentespresso.com/download/sync/de1plus/manifest.txt
 
-if [ $? -ne 0 ]
-then
-  exit 1
-fi
-
 echo "Checking if there are differences..."
-git diff --exit-code manifest.txt
+git diff --exit-code manifest.txt || HAS_CHANGES=$?
 
-if [ $? -eq 0 ]
+if [ $HAS_CHANGES -eq 0 ]
 then
   echo "No changes!"
 else

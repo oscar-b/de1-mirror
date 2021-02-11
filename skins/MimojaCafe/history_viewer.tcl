92c7178143fa9cc3a61263f73d797f7723c91484
Decent Espresso <mirror@decentespresso.com>
2021-02-11 05:10:35 +0000

    2021-02-11 04:46:29 UTC

--- skins/MimojaCafe/history_viewer.tcl
+++ skins/MimojaCafe/history_viewer.tcl
@@ -3,17 +3,20 @@
 # Logic
 #
 
-array set ::history {}
+set ::history_match_profile 0
 
 blt::vector create history_elapsed history_pressure_goal history_flow_goal history_temperature_goal
 
-blt::vector create history_pressure history_flow
+blt::vector create history_pressure history_flow history_flow_weight
 blt::vector create history_weight
 
 blt::vector create history_state_change
 blt::vector create history_resistance_weight history_resistance
 
 blt::vector create history_flow_delta_negative_2x history_flow_delta_negative history_pressure_delta
+
+blt::vector create history_temperature_basket history_temperature_mix  history_temperature_goal
+
 
 array set ::past_shot {}
 
@@ -27,14 +30,20 @@ proc fill_history_listbox {} {
 	#puts "fill_history_listbox $widget"
 	set widget $::history_widget
 	$widget delete 0 99999
+	$::history_widget delete 0 99999
 	set cnt 0
+	set ::history_files {}
 
-	set ::history_files [lsort -dictionary -decreasing [glob -nocomplain -tails -directory "[homedir]/history/" *.shot]]
+	set files [lsort -dictionary -decreasing [glob -nocomplain -tails -directory "[homedir]/history/" *.shot]]
 
-    foreach shot_file $::history_files {
+    foreach shot_file $files {
         set tailname [file tail $shot_file]
         set newfile [file rootname $tailname]
         set fname "history/$newfile.csv"
+
+		if {$cnt == $::iconik_settings(max_history_items)} {
+			break;
+		}
 
 		array unset -nocomplain shot
 		catch {
@@ -44,15 +53,35 @@ proc fill_history_listbox {} {
 			msg "Corrupted shot history item: 'history/$shot_file'"
 			continue
 		}
-		set dbg [array get shot]
-		msg "Read history item: $fname"
+		array set shot_settings $shot(settings)
 
-		$widget insert $cnt $newfile
-		incr cnt
+		if {$::history_match_profile == 1 && $shot_settings(profile_title) ne $::settings(profile_title)} {
+			continue
+		} else {
+			$widget insert $cnt $newfile
+			incr cnt
+			lappend ::history_files $shot_file
+		}
 	}
 
 	set $::history_widget widget
 }
+
+proc god_shot_from_history {} {
+	set stepnum [$::history_widget curselection]
+	if {$stepnum == ""} {
+		return
+	}
+    set ::settings(god_espresso_pressure) [history_pressure range 0 end]
+    set ::settings(god_espresso_temperature_basket) [history_temperature_basket range 0 end]
+    set ::settings(god_espresso_flow) [history_flow range 0 end]
+    set ::settings(god_espresso_flow_weight) [history_flow_weight range 0 end]
+    set ::settings(god_espresso_elapsed) [history_elapsed range 0 end]
+
+    save_settings
+    god_shot_reference_reset
+}
+
 
 proc past_title {} {
 	if {[info exists ::past_shot(settings)] == 1} {
@@ -63,7 +92,12 @@ proc past_title {} {
 	return ""
 }
 
-
+proc load_history_field {target field} {
+	if {[info exists ::past_shot($field)] == 1} {
+		set value [set ::past_shot($field)]
+		$target set $value
+	}
+}
 
 proc show_past_shot {} {
 	set stepnum [$::history_widget curselection]
@@ -78,23 +112,24 @@ proc show_past_shot {} {
 
 	msg "Read shot $fn"
 
-	history_elapsed set $::past_shot(espresso_elapsed)
-	history_pressure_goal set $::past_shot(espresso_pressure_goal)
-	history_flow_goal set $::past_shot(espresso_flow_goal)
-	history_pressure set $::past_shot(espresso_pressure)
-	history_flow set $::past_shot(espresso_flow)
-	history_weight set $::past_shot(espresso_weight)
-
+	load_history_field history_elapsed            espresso_elapsed
+	load_history_field history_pressure_goal      espresso_pressure_goal
+	load_history_field history_pressure           espresso_pressure
+	load_history_field history_flow_goal          espresso_flow_goal
+	load_history_field history_flow               espresso_flow
+	load_history_field history_flow_weight        espresso_flow_weight
+	load_history_field history_weight             espresso_weight
+	load_history_field history_temperature_basket espresso_temperature_basket
+	load_history_field history_temperature_mix    espresso_temperature_mix
+	load_history_field history_temperature_goal   espresso_temperature_goal
 	# New 1.34.5 shot fields
-	history_temperature_goal set $::past_shot(espresso_temperature_goal)
-	history_state_change set $::past_shot(espresso_state_change)
-	history_resistance_weight set $::past_shot(espresso_resistance_weight)
-	history_resistance set $::past_shot(espresso_resistance)
-	history_flow_delta_negative_2x set $::past_shot(espresso_flow_delta_negative_2x)
-	history_flow_delta_negative set $::past_shot(espresso_flow_delta_negative)
-	history_pressure_delta set $::past_shot(espresso_pressure_delta)
-
-	#msg $::past_shot(settings)
+	load_history_field history_temperature_goal       espresso_temperature_goal
+	load_history_field history_state_change           espresso_state_change
+	load_history_field history_resistance_weight      espresso_resistance_weight
+	load_history_field history_resistance             espresso_resistance
+	load_history_field history_flow_delta_negative_2x espresso_flow_delta_negative_2x
+	load_history_field history_flow_delta_negative    espresso_flow_delta_negative
+	load_history_field history_pressure_delta         espresso_pressure_delta
 }
 
 #
@@ -103,6 +138,7 @@ proc show_past_shot {} {
 
 add_background "history"
 
+add_de1_variable "history" 680 60 -width [rescale_x_skin 900]  -text "" -font $::font_big -fill [theme primary_light] -anchor "nw" -justify "center" -state "hidden" -textvariable {[past_title]}
 
 add_de1_widget "history" graph 680 240 {
 	#Target
@@ -127,7 +163,9 @@ add_de1_widget "history" graph 680 240 {
 	$widget axis configure y -color [theme background_text] -tickfont Helv_7 -min 0.0 -max 12 -subdivisions 5 -majorticks {0 1 2 3 4 5 6 7 8 9 10 11 12}  -hide 0;
 } -plotbackground [theme background] -width [rescale_x_skin 1860] -height [rescale_y_skin 1180] -borderwidth 1 -background [theme background] -plotrelief flat
 
-add_de1_widget "history" listbox 80	80 {
+add_de1_widget "history" checkbutton 80 80 {} -text [translate "Match current profile"] -indicatoron true  -font $::font_tiny -bg [theme background] -anchor nw -foreground [theme background_text] -variable ::history_match_profile -borderwidth 0 -selectcolor [theme background] -highlightthickness 0 -activebackground [theme background]  -bd 0 -activeforeground [theme background_text] -relief flat -bd 0 -command {fill_history_listbox}
+
+add_de1_widget "history" listbox 80	180 {
 	set ::history_widget $widget
 	bind $::history_widget <<ListboxSelect>> ::show_past_shot
 	fill_history_listbox
@@ -140,6 +178,5 @@ proc set_history_scrollbar_dimensions {} {
 	set_scrollbar_dimensions $::history_scrollbar $::history_widget
 }
 
-add_de1_variable "history" 80 1360 -width [rescale_x_skin 380]  -text "" -font $::font_big -fill [theme primary_light] -anchor "nw" -justify "center" -state "hidden" -textvariable {[past_title]}
-
-create_button "history" 580 1440 1880 1560 [translate "Done"] $::font_tiny [theme button_tertiary] [theme button_text_light] { say [translate "settings"] $::settings(sound_button_in); page_to_show_when_off "off" }
+create_button "history" 580 1440 1160 1560 [translate "Make Reference / Godshot"] $::font_tiny [theme button_tertiary] [theme button_text_light] { say [translate "settings"] $::settings(sound_button_in); god_shot_from_history; page_to_show_when_off "off" }
+create_button "history" 1210 1440 1880 1560 [translate "Done"] $::font_tiny [theme button_tertiary] [theme button_text_light] { say [translate "settings"] $::settings(sound_button_in); page_to_show_when_off "off" }

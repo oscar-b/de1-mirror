1b44a80b42014439337e96a4bc7d680391b64897
Decent Espresso <mirror@decentespresso.com>
2021-04-23 04:18:00 +0000

    2021-04-23 03:20:20 UTC

--- plugins/SDB/plugin.tcl
+++
@@ -1,9 +0,0 @@
-#######################################################################################################################
-### A Decent DE1 app plugin to keep a synchronized SQLite database of shots and manage shots history.
-###
-### https://github.com/ebengoechea/de1app_plugin_SDB
-### This code is released under GPLv3 license. 
-#######################################################################################################################
-set plugin_name "SDB"
-
-source "[plugin_directory]/${plugin_name}/${plugin_name}.tcl"
